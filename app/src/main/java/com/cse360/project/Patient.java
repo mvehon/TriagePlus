1ec0ea25c9229503d6cb35f0a451bb195614c914
Matthew Vehon <mvehon@asu.edu>
2015-04-21 17:17:26 -0700

    Added server communication

--- app/src/main/java/com/cse360/project/Patient.java
+++ app/src/main/java/com/cse360/project/Patient.java
@@ -1,9 +1,12 @@
 package com.cse360.project;
 
+import com.parse.FindCallback;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Patient implements Serializable {
 	private String first_name;
@@ -86,6 +89,24 @@ public class Patient implements Serializable {
         pt.saveEventually();
     }
 
+    public void updateOnServer(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Patient");
+        query.whereEqualTo("username", first_name+last_name);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> parseObjects, com.parse.ParseException e) {
+                if (e == null) {
+                    if(parseObjects.size()>0){
+                        ParseObject pt = parseObjects.get(0);
+                        pt.add("symptom0", getSymptom0().get(getSymptom0().size()-1));
+                        pt.add("symptom1", getSymptom1().get(getSymptom1().size()-1));
+                        pt.add("symptom2", getSymptom2().get(getSymptom2().size()-1));
+                        pt.saveInBackground();
+                    }
+                }
+            }
+        });
+    }
+
 	public void addValues(int[] pains){
 		for(int i=0; i<pains.length;i++){
 			switch(i){
@@ -101,4 +122,9 @@ public class Patient implements Serializable {
 			}
 		}
 	}
+
+    @Override
+    public String toString() {
+        return first_name + " " + last_name;
+    }
 }
