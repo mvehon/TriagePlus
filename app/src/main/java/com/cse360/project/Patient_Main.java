ba19164bf1cfc57c6d39d65779a90417f2ce0505
JoeyCim <joey.cimochowski1995@gmail.com>
2015-04-23 23:02:16 -0700

    Adding patient prescription history table

--- app/src/main/java/com/cse360/project/Patient_Main.java
+++ app/src/main/java/com/cse360/project/Patient_Main.java
@@ -4,20 +4,29 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Patient_Main extends Activity{
 
-    SharedPreferences prefs;
-    Button submitButton;
-    Patient curUser;
+    private SharedPreferences prefs;
+    private Button submitButton;
+    private Patient curUser;
+    private String[] tableWords = {"Prescription Name", "Fill Date", "Duration"};
+    private TableLayout table;
+    private ArrayList<Prescription> prescriptionList; //List of prescriptions for this patient
+    private static final int NUM_COLUMNS = 3; //Number of columns in the prescription table
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +55,78 @@ public class Patient_Main extends Activity{
             }
         });
 
-        //TODO ADD IN PRESCRIPTION-VIEWING
+        table = (TableLayout)findViewById(R.id.table);
+        TableRow row = new TableRow(this); //First row containing titles
 
+        //Add in three "header" columns
+        for(int i = 0; i < NUM_COLUMNS; i++) {
+            TextView text = new TextView(this); //Entry text
+            text.setText(tableWords[i]);
+            text.setBackgroundColor(Color.LTGRAY);
+            row.addView(text);
+
+            //Create padding between text entries; add text to appropriate column
+            TableRow.LayoutParams params =
+                    (TableRow.LayoutParams)text.getLayoutParams();
+            params.column= i;
+            params.span = 1;
+            params.setMargins(3, 3, 3, 3);
+            params.width = TableRow.LayoutParams.MATCH_PARENT;
+            params.height = TableRow.LayoutParams.WRAP_CONTENT;
+            text.setPadding(3, 3, 3, 3);
+            text.setLayoutParams(params);
+            row.setBackgroundResource(R.drawable.row_border);
+        }
+        table.addView(row,
+                new TableLayout.LayoutParams
+                        (TableLayout.LayoutParams.WRAP_CONTENT,
+                                TableLayout.LayoutParams.WRAP_CONTENT));
+
+        prescriptionList = new ArrayList<Prescription>();
+
+        //Some dummy test data
+        prescriptionList.add(new Prescription("Goofballs", "1/23/24", 30));
+        prescriptionList.add(new Prescription("Vicodin", "1/22/24", 20));
+        prescriptionList.add(new Prescription("Lunesta", "1/20/24", 40));
+
+        for (int j = 0; j < prescriptionList.size(); j++) {
+            TableRow rxRow = new TableRow(this); //Row showing prescription info
+            for(int i = 0; i < NUM_COLUMNS; i++) {
+                TextView text = new TextView(this);
+
+                //Determine what content to display based on current loop index
+                switch(i) {
+                    case 0: //Prescription name
+                        text.setText(prescriptionList.get(j).getRx_name());
+                        break;
+                    case 1: //Prescription fill date
+                        text.setText(prescriptionList.get(j).getFill_date());
+                        break;
+                    case 2: //Prescription duration
+                        text.setText(prescriptionList.get(j).getDuration() + " days.");
+                        break;
+                }
+                text.setBackgroundColor(Color.LTGRAY);
+                rxRow.addView(text);
+
+                //Create padding between text entries; add text to appropriate column
+                TableRow.LayoutParams params =
+                        (TableRow.LayoutParams)text.getLayoutParams();
+                params.column= i;
+                params.span = 1;
+                params.setMargins(3, 3, 3, 3);
+                params.width = TableRow.LayoutParams.MATCH_PARENT;
+                params.height = TableRow.LayoutParams.WRAP_CONTENT;
+                text.setPadding(3, 3, 3, 3);
+                text.setLayoutParams(params);
+                rxRow.setBackgroundResource(R.drawable.row_border);
+            }
+            table.addView(rxRow,
+                    new TableLayout.LayoutParams
+                            (TableLayout.LayoutParams.WRAP_CONTENT,
+                                    TableLayout.LayoutParams.WRAP_CONTENT));
+        }
+        super.onCreate(savedInstanceState);
     }
 
     @Override
